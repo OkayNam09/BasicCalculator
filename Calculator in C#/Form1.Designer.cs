baccec70d77e44125933d90b685ee19a31d8cbff
OkayNam09 <namron.blanza@lpunetwork.edu.ph>
2024-11-12 19:00:03 +0800

    Add project files.

---
+++ Calculator in C#/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace BasicCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFirstNumber = new System.Windows.Forms.TextBox();
+            this.txtSecondNumber = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFirstNumber
+            // 
+            this.txtFirstNumber.Location = new System.Drawing.Point(135, 6);
+            this.txtFirstNumber.Name = "txtFirstNumber";
+            this.txtFirstNumber.Size = new System.Drawing.Size(199, 20);
+            this.txtFirstNumber.TabIndex = 0;
+            // 
+            // txtSecondNumber
+            // 
+            this.txtSecondNumber.Location = new System.Drawing.Point(135, 32);
+            this.txtSecondNumber.Name = "txtSecondNumber";
+            this.txtSecondNumber.Size = new System.Drawing.Size(199, 20);
+            this.txtSecondNumber.TabIndex = 1;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(135, 107);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(199, 20);
+            this.txtResult.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter First Number";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter Second Number";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(37, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Result";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(15, 66);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 6;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(96, 65);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(75, 23);
+            this.btnSubtract.TabIndex = 7;
+            this.btnSubtract.Text = "Subtract";
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(177, 65);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(75, 23);
+            this.btnMultiply.TabIndex = 8;
+            this.btnMultiply.Text = "Multiply";
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(259, 65);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(75, 23);
+            this.btnDivide.TabIndex = 9;
+            this.btnDivide.Text = "Divide";
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(259, 133);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 10;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 167);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSecondNumber);
+            this.Controls.Add(this.txtFirstNumber);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFirstNumber;
+        private System.Windows.Forms.TextBox txtSecondNumber;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnClear;
+    }
+}
+
---
+++ Calculator in C#/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace BasicCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+
+            if (!inputValidation(out num1, out num2)) 
+                return;
+
+            double result = num1 + num2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void btnSubtract_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+
+            if (!inputValidation(out num1, out num2))
+                return;
+
+            double result = num1 - num2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void btnMultiply_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+
+            if (!inputValidation(out num1, out num2))
+                return;
+
+            double result = num1 * num2;
+            txtResult.Text = result.ToString();
+        }
+
+        private void btnDivide_Click(object sender, EventArgs e)
+        {
+            double num1, num2;
+
+            if (!inputValidation(out num1, out num2))
+                return;
+
+            if (num2 == 0)
+            {
+                MessageBox.Show("Cannot divide by zero", "Invalid Operation", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            double result = num1 / num2;
+            txtResult.Text = result.ToString();
+
+        }
+
+        private bool inputValidation(out double num1, out double num2)
+        {
+            num1 = 0;
+            num2 = 0;
+
+            if(!double.TryParse(txtFirstNumber.Text, out num1))
+            {
+                MessageBox.Show("Please enter a valid number for the first number", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+
+            if (!double.TryParse(txtSecondNumber.Text, out num2))
+            {
+                MessageBox.Show("Please enter a valid number for the second number", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtFirstNumber.Clear();
+            txtSecondNumber.Clear();
+            txtResult.Clear();
+        }
+    }
+}
